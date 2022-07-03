708ba164320ca3d4e86a62a86b6bfd338ce09fa0
AHLaboni <75937942+AHLaboni@users.noreply.github.com>
2022-07-04 00:40:45 +0600

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
+session_start();
 
-	session_start();
-	session_unset();
-	session_destroy();
+if(session_destroy())
+{
+    header("location: ../View/home.php");
+}
 
-	header("Location: login.php");
 ?>
